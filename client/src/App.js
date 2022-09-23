c2198a15202e2eed3f46703590e9c0d29d8f242b
Helen Langridge <langridge.helen@outlook.com>
2022-09-23 14:57:04 +0100

    api working

--- client/src/App.js
+++ client/src/App.js
@@ -15,17 +15,28 @@ function App() {
     })
   }, [])
 
-  const addNewEntry = (entry)=>{
-    addEntry(entry)
-    .then(savedEntry => setEntries([...entries, savedEntry]))
+
+  useEffect(() => {
+    getRecipes();
+  }, [])
+
+  const getRecipes = function(){
+      fetch('https://api.edamam.com/api/recipes/v2?type=public&q=pasta&app_id=f56f77fd&app_key=f4f4f1660ed0c0457723b124104dcc76%20%09')
+      .then(res => res.json())
+      .then(recipes => setRecipes(recipes.hits))
   }
+
+  // const addNewEntry = (entry)=>{
+  //   addEntry(entry)
+  //   .then(savedEntry => setEntries([...entries, savedEntry]))
+  // }
 
   
   
 
   return (
     <div>
-    <Dashboard entries={entries}/>
+    <Dashboard entries={entries} recipes={recipes}/>
     </div>
   );
 }
--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-const Dashboard = ({entries}) => {
+const Dashboard = ({entries, recipes}) => {
   if(!entries) return <h2>Loading...</h2>
+  if(!recipes) return <h2>Loading...</h2>
+
+  console.log(recipes)
 
   const entriesList = entries.map((entry)=>{
     return (
@@ -13,10 +16,19 @@ const Dashboard = ({entries}) => {
     )
   })
 
+  const recipesList = recipes.map((recipe)=>{
+    return (
+      <li>
+        <h2>{recipe.recipe.label}</h2>
+      </li>
+    )
+  })
+
   return (
     <div>
       <ul>
         {entriesList}
+        {recipesList}
       </ul>
     </div>
   )
