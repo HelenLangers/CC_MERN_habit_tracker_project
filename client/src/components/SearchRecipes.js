816a9ebe65630f15039aac571f0190c249d2b967
Helen Langridge <langridge.helen@outlook.com>
2022-09-25 16:56:52 +0100

    animated search bar added

--- client/src/components/SearchRecipes.js
+++ client/src/components/SearchRecipes.js
@@ -1,10 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import RecipeList from './RecipeList'
 import './SearchRecipes.css';
 
 const SearchRecipes = ({recipes, setQuery }) => {
 
   const [search, setSearch] = useState('')
+  const [isHovered, setIsHovered] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const targetRef = useRef(null);
+  const showSearchInput = isHovered || isFocused;
+
+  useEffect(() => {
+    targetRef.current.value = "";
+  }, [showSearchInput]);
+
   if(!recipes) return <h2>Loading...</h2>
 
   const submitSearch = (event) => {
@@ -18,20 +27,26 @@ const SearchRecipes = ({recipes, setQuery }) => {
 
   return (
     <div className="search-page">
-    <div className="wrapper">
-      <form onSubmit={submitSearch} className="search-form">
-      <i className="fa-solid fa-magnifying-glass search-icon"></i>
+    {/* <div className="wrapper"> */}
+      <form onSubmit={submitSearch} className="search-form"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      hover={showSearchInput}>
+      {/* <i className="fa-solid fa-magnifying-glass search-icon"></i> */}
         <input className="search-bar" 
         type="text" 
-        placeholder="What do you want to eat?"
+        placeholder="What do you want to cook?"
         value={search} 
-        onChange={handleSearchBarChange}></input>
+        onChange={handleSearchBarChange}
+        ref={targetRef} showSearchInput={showSearchInput}></input>
         <button className="search-button" 
         type="submit">
         Search
         </button>
       </form>
-    </div>
+    {/* </div> */}
 
     <div className="search-results">
       {recipes.map((recipe) => (
