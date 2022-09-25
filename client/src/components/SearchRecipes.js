844cc9e3a30fba595a6f8660cd5098c0290af949
Helen Langridge <langridge.helen@outlook.com>
2022-09-25 19:16:52 +0100

    reset search

--- client/src/components/SearchRecipes.js
+++ client/src/components/SearchRecipes.js
@@ -11,6 +11,7 @@ const SearchRecipes = ({recipes, setQuery }) => {
   const submitSearch = (event) => {
     event.preventDefault()
     setQuery(search)
+    setSearch('')
   }
 
   const handleSearchBarChange = (event) => {
