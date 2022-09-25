03a71f0b1ccee48e29b350998f54636e50fed6b8
Helen Langridge <langridge.helen@outlook.com>
2022-09-25 19:07:03 +0100

    simple animated search bar

--- client/src/components/SearchRecipes.js
+++ client/src/components/SearchRecipes.js
@@ -1,18 +1,10 @@
-import React, {useState, useEffect, useRef} from 'react'
-import RecipeList from './RecipeList'
+import React, {useState} from 'react';
+import RecipeList from './RecipeList';
 import './SearchRecipes.css';
 
 const SearchRecipes = ({recipes, setQuery }) => {
 
   const [search, setSearch] = useState('')
-  const [isHovered, setIsHovered] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
-  const targetRef = useRef(null);
-  const showSearchInput = isHovered || isFocused;
-
-  useEffect(() => {
-    targetRef.current.value = "";
-  }, [showSearchInput]);
 
   if(!recipes) return <h2>Loading...</h2>
 
@@ -25,28 +17,23 @@ const SearchRecipes = ({recipes, setQuery }) => {
     setSearch(event.target.value)
   }
 
+
   return (
     <div className="search-page">
-    {/* <div className="wrapper"> */}
-      <form onSubmit={submitSearch} className="search-form"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-      hover={showSearchInput}>
-      {/* <i className="fa-solid fa-magnifying-glass search-icon"></i> */}
+    <div className="wrapper">
+      <form onSubmit={submitSearch} className="search-form">
         <input className="search-bar" 
         type="text" 
-        placeholder="What do you want to cook?"
         value={search} 
-        onChange={handleSearchBarChange}
-        ref={targetRef} showSearchInput={showSearchInput}></input>
+        placeholder="Search..."
+        onChange={handleSearchBarChange}/>
+        <i className="fa-solid fa-magnifying-glass"></i>
         <button className="search-button" 
         type="submit">
         Search
         </button>
       </form>
-    {/* </div> */}
+    </div>
 
     <div className="search-results">
       {recipes.map((recipe) => (
