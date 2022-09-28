c0e35b80ccf261bf2437daf39431ce05e9ac8d5f
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-09-28 18:52:04 +0100

    changes

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
-import {useLocation, useNavigate} from "react-router-dom";
-import './Form.css'
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./Form.css";
 
 const Form = ({ onEntrySubmit }) => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const location = useLocation();
   const state = location.state;
@@ -15,62 +14,70 @@ const Form = ({ onEntrySubmit }) => {
 
   const handleRecipeChange = (e) => {
     setRecipe(e.target.value);
-  }
+  };
 
   const handleDateChange = (e) => {
     setDate(e.target.value);
-  }
+  };
 
   const handleNotesChange = (e) => {
     setNotes(e.target.value);
-  }
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const recipeToSubmit = recipe;
     const dateToSubmit = date;
-    const notesToSubmit = notes
-    
+    const notesToSubmit = notes;
+
     onEntrySubmit({
-      recipeName: (recipeToSubmit ? recipeToSubmit : state.recipe.recipe.label),
+      recipeName: recipeToSubmit ? recipeToSubmit : state.recipe.recipe.label,
       date: dateToSubmit,
-      notes: notesToSubmit
+      notes: notesToSubmit,
     });
 
     setRecipe("");
     setDate("");
     setNotes("");
 
-    navigate('/entries')
+    navigate("/entries");
 
     // console.log(recipeToSubmit, dateToSubmit, notes)
-  }
-  
+  };
+
   return (
     <>
-    <h1>Step 2: choose a recipe below. Cook it up and add it to your meals!</h1>
-    <form onSubmit={handleFormSubmit} id="form-container">
-      <input type="text" className="form-input" 
-      placeholder='Recipe'
-      value={recipe || state.recipe.recipe.label}
-      onChange={handleRecipeChange} contentEditable></input>
+      <h3>Mmm, delicious!</h3>
+      <form onSubmit={handleFormSubmit} id="form-container">
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Recipe"
+          value={recipe || state.recipe.recipe.label}
+          onChange={handleRecipeChange}
+          contentEditable
+        ></input>
 
-      <input type="date" className="form-input" 
-      placeholder='Date'
-      value={date}
-      onChange={handleDateChange}></input>
+        <input
+          type="date"
+          className="form-input"
+          placeholder="Date"
+          value={date}
+          onChange={handleDateChange}
+        ></input>
 
-      <input id='notes' type="text" className="form-input"  
-      placeholder='Notes'
-      value={notes}
-      onChange={handleNotesChange}></input>
-      
-      <input type="submit"
-      value="Submit"></input>
+        <input
+          id="notes"
+          type="text"
+          className="form-input"
+          placeholder="Notes"
+          value={notes}
+          onChange={handleNotesChange}
+        ></input>
 
-    </form>
+        <input type="submit" value="Add entry"></input>
+      </form>
     </>
-    )
-}
-export default Form
- 
+  );
+};
+export default Form;
